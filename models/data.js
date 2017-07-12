41697f4529a2ee91f00ad6c688f271ceabcd0602
Ary Davani <arydavani@gmail.com>
2017-07-12 15:02:19 -0400

    added data

---
+++ models/data.js
@@ -0,0 +1,8 @@
+
+module.exports = [
+  { id: 1, description: 'lays chips', cost: 1.00 },
+  { id: 2, description: 'snickers', cost: 1.75 },
+  { id: 3, description: 'starburts', cost: 1.25 },
+  { id: 4, description: 'doritos', cost: 1.00 },
+  { id: 5, description: 'gum', cost: 0.75 },
+]
